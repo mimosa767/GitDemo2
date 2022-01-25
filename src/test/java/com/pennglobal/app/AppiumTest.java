77b3e1e2e8c50a077e2b2861749a3a7cdd2fcca6
Stephen <penn@stephenpenn.com>
2022-01-25 17:59:51 -0500

    commit message

---
+++ src/test/java/com/pennglobal/app/AppiumTest.java
@@ -0,0 +1,15 @@
+package com.pennglobal.app;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+    @Test
+    public void AppAndroid () {
+        System.out.println("AppAndroid");
+    }
+    @Test
+    public void IOSApps () {
+        System.out.println("IOSApps");
+    }
+}
---
+++ src/test/java/com/pennglobal/app/SeleniumTest.java
@@ -0,0 +1,15 @@
+package com.pennglobal.app;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @Test
+    public void BrowserAutomation() {
+        System.out.println("BrowserAutomation");
+    }
+    @Test
+    public void elementsUi() {
+        System.out.println("ElementUI");
+    }
+}
